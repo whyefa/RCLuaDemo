647e4cf679bcaa976a139f552bef65f1bdcf50bf
Broken <whyefa@163.com>
2016-06-30 12:11:27 +0800

    v0.0.1
    1

---
+++ RCLuaDemo/Lua/FlexBox.lua
@@ -0,0 +1,12 @@
+
+LIGHT_GRAY = 0xf5edb3
+RED_COLOR = 0xf61d4b
+BLUE_COLOR = 0x26a96d
+GREEN_COLOR = 0x05a5d1
+MAGENTA_COLOR = 0Xff00ff
+MACCASIN_COLOR = 0xffe4b5
+OLIVEDRAB_COLOR = 0X6b8e23
+
+w, h = System.screenSize()
+
+contentView = View()
